4fe5af12d53b3bfb83db617b797302c582f8260f
Josh Young <joshua.jjyoung@gmail.com>
2018-08-07 12:43:22 -0500

    Adding example files from C++ learning exercises

---
+++ CPP_AIO/BookI/Chapter03/EqualityCheck/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  int first = 0;
+  int second = 0;
+  string result;
+  result = first == second ? "equal" : "not equal";
+  cout << result << endl;
+  return 0;
+}
---
+++ CPP_AIO/BookI/Chapter03/IndividualCharacter/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  string mystring;
+  mystring = "abcdef";
+  cout << mystring[4] << endl;
+  return 0;
+}
